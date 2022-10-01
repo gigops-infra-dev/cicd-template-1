c741763086542f1996e8bfc1d2c9492b46c429c0
ezqy <ezaki@tc3.co.jp>
2022-10-01 22:08:01 +0900

    test31

--- .github/actions/split-branch/main.sh
+++ .github/actions/split-branch/main.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set +e
-set -x
 
 isTmp() {
   if [ "$TARGET_DIR" == 'tmp' ]; then
@@ -37,6 +36,7 @@ main(){
     git commit -m "Merge pr/${BASE_REF}/${HEAD_REF#feature/}_${TARGET_DIR}"
   fi
   git reset --hard HEAD
+  git fetch --all
   git rebase --onto staging ${HEAD_REF} pr/${BASE_REF}/${HEAD_REF#feature/}_${TARGET_DIR}
   echo "push"
   git push -f origin pr/${BASE_REF}/${HEAD_REF#feature/}_${TARGET_DIR}
